385f72da2a1e475144b27b405075544af33e2d7a
Gordon Gest <gordon.gest@gmail.com>
2022-09-22 21:59:51 -0600

    move /cards middleware back to original order, add notes, remove test query

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,10 +31,66 @@ const graphQLClient = new GraphQLClient('https://api.us.test.highnoteplatform.co
   }
 });
 
+// app.use is consuming all requests even when other endpoints are defined first, may need to narrow scope or handle methods directly
+app.use('/', async (req: Request, res: Response, next: NextFunction) => {
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    return res.status(204).send('');
+  } else if (req.method === 'GET') {
+    // fork here
+    // check some property of /cards request and call next()
+    const cacheResults = await redisClient.get('users');
+    if (cacheResults) {
+      console.log('cached users', { data: JSON.parse(cacheResults) });
+      return res.status(200).json({
+        data: JSON.parse(cacheResults),
+      });
+    } else {
+      console.log('no cached users');
+      return res.status(200).json({
+        data: [],
+      });
+    }
+  } else {
+    const cacheResults = await redisClient.get('users');
+    if (cacheResults) {
+      await redisClient.set(
+        'users',
+        JSON.stringify([...JSON.parse(cacheResults), req.body])
+      );
+      const updatedData = await redisClient.get('users');
+
+      console.log('updated cached users', { data: req.body });
+      return res.status(200).json({
+        data: JSON.parse(updatedData),
+      });
+    } else {
+      await redisClient.set('users', JSON.stringify([req.body]));
+      const updatedData = await redisClient.get('users');
+
+      console.log('initiated users', { data: JSON.parse(updatedData) });
+      return res.status(200).json({
+        data: JSON.parse(updatedData),
+      });
+    }
+  }
+  // can't make it here because all requests are consumed and sending responses
+  next();
+});
+
+/**
+ * Second Phase Stub Out
+ *
+ * TODO: implement endpoint
+ */
 app.use(
     '/cards/:cardId',
     async (req: Request, res: Response, next: NextFunction) => {
-      console.log('cardId:', req.params.cardId)
       // make request to GraphQL resource
       // if successful, return results as JSON
 
@@ -147,70 +203,5 @@ app.use(
       next();
     }
 );
-
-// app.use is consuming all requests even when other endpoints are defined first, may need to narrow scope or handle methods directly
-app.use('/', async (req: Request, res: Response, next: NextFunction) => {
-  // /* GraphQLClient test query */
-  // const testQuery = gql`
-  //     query Ping {
-  //       ping
-  //     }
-  //   `
-  // const results = await graphQLClient.request(testQuery);
-  // console.log("Highnote test query results:", results);
-
-  res.set('Access-Control-Allow-Origin', '*');
-
-  if (req.method === 'OPTIONS') {
-    // Send response to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'GET');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('Access-Control-Max-Age', '3600');
-    return res.status(204).send('');
-  } else if (req.method === 'GET') {
-    const cacheResults = await redisClient.get('users');
-    if (cacheResults) {
-      console.log('cached users', { data: JSON.parse(cacheResults) });
-      return res.status(200).json({
-        data: JSON.parse(cacheResults),
-      });
-    } else {
-      console.log('no cached users');
-      return res.status(200).json({
-        data: [],
-      });
-    }
-  } else {
-    const cacheResults = await redisClient.get('users');
-    if (cacheResults) {
-      await redisClient.set(
-        'users',
-        JSON.stringify([...JSON.parse(cacheResults), req.body])
-      );
-      const updatedData = await redisClient.get('users');
-
-      console.log('updated cached users', { data: req.body });
-      return res.status(200).json({
-        data: JSON.parse(updatedData),
-      });
-    } else {
-      await redisClient.set('users', JSON.stringify([req.body]));
-      const updatedData = await redisClient.get('users');
-
-      console.log('initiated users', { data: JSON.parse(updatedData) });
-      return res.status(200).json({
-        data: JSON.parse(updatedData),
-      });
-    }
-  }
-  // can't make it here because all requests are consumed and sending responses
-  next();
-});
-
-/**
- * Second Phase Stub Out
- *
- * TODO: implement endpoint
- */
 
 export default app;
